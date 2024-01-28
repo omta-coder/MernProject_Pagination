91becb0025b4e98428cf8353c47595c414427909
omta <omtadayma2001@gmail.com>
2024-01-28 13:05:51 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Headers/Headers";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Edit from "./pages/Edit/Edit";
+import Profile from "./pages/Profile/Profile";
+import {Route, Routes} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/edit/:id' element={<Edit />} />
+        <Route path='/userprofile/:id' element={<Profile />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "./components/context/ContextProvider.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextProvider>
+);
